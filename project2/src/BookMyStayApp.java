6a238ea0527d95f1ad2b7322d855310eeb55bc76
adyavinod221@gmail.com <malvisood@gmail.com>
2026-03-13 09:42:10 +0530

    UC1 DONE

---
+++ project2/src/BookMyStayApp.java
@@ -0,0 +1,13 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        // Print welcome message and application details
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay App!");
+        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("=======================================");
+    }
+}
+
+
+
+
